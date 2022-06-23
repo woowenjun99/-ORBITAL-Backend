6ddaf84f87682c45c44d9cfec0cbdcd1ccf8805a
woowenjun99 <woowenjun99@gmail.com>
2022-06-23 10:09:46 +0800

    Wrote documentation

---
+++ functions/Cloud Functions/filterAndSearch.js
@@ -0,0 +1,3 @@
+const functions = require('firebase-functions');
+
+exports.filterAndSearch = functions.https.onCall(async (data, context) => {});
--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,11 @@ const { home } = require('./API/home');
 const { item } = require('./API/item');
 const { getUserListing } = require('./API/history');
 const { marketplace } = require('./API/marketplace');
-const { getHomepageListing } = require('./Cloud Functions/Home/getHomepageListings');
+const { getHomepageListings } = require('./Cloud Functions/getHomepageListings');
 
 /// HTTP Requests for Marcus
 exports.home = home;
 exports.item = item;
 exports.getUserListing = getUserListing;
 exports.marketplace = marketplace;
-exports.getHomepageListing = getHomepageListing;
+exports.getHomepageListings = getHomepageListings;
